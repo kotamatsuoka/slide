ece58c3b891f66607ef8b94de47f45bb8f0567c4
Matsuoka Kota <kota11250910@gmail.com>
2020-06-21 12:38:18 +0900

    add: sample code

---
+++ unittest/leap_year/01/test_my_year.py
@@ -0,0 +1,50 @@
+"""
+設計のレビューをしてもらえるように、関数の呼び出し方と、テストケースのみを書いてみる
+
+【閏年問題】
+以下、3つのメソッドを持つMyYearクラスの実装について考える
+
+1. 整数の引数を持ち、その引数を保持する
+2. 保持した値を文字列として返す
+3. 保持した値が閏年ならばtrueを返し、それ以外はfalseを返す
+"""
+
+import pytest
+
+
+class MyYear:
+    def __init__(self):
+        pass
+
+    def to_string(self):
+        pass
+
+    def is_leap(self):
+        pass
+
+
+def test_initialize():
+    target_year = 2020
+    actual = MyYear(target_year).value
+    expected = target_year
+
+    assert actual == expected
+
+
+def test_to_datetime():
+    target_year = 2020
+    actual = MyYear(target_year).to_string()
+    expected = str(target_year)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize("target_year, expected", [
+    (2020, True),
+    (2019, False),
+    (2100, False),
+])
+def test_is_leap(target_year, expected):
+    actual = MyYear(target_year).is_leap()
+
+    assert actual == expected
---
+++ unittest/leap_year/03/test_my_year.py
@@ -0,0 +1,54 @@
+"""
+「Green」->「Refactor」
+
+【閏年問題】
+以下、3つのメソッドを持つMyYearクラスの実装について考える
+
+1. 整数の引数を持ち、その引数を保持する
+2. 保持した値を文字列として返す、to_string()を持つ
+3. 保持した値が閏年ならばtrueを返し、それ以外はfalseを返す
+"""
+
+import pytest
+
+
+class MyYear:
+    def __init__(self, value):
+        self.value = value
+
+    def to_string(self):
+        return str(self.value)
+
+    def is_leap(self):
+        if self.value % 4 != 0:
+            return False
+        if self.value % 100 == 0 and self.value % 400 != 0:
+            return False
+        return True
+
+
+def test_initialize():
+    target_year = 2020
+    actual = MyYear(target_year).value
+    expected = target_year
+
+    assert actual == expected
+
+
+def test_to_datetime():
+    target_year = 2020
+    actual = MyYear(target_year).to_string()
+    expected = str(target_year)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize("target_year, expected", [
+    (2020, True),
+    (2019, False),
+    (2100, False),
+])
+def test_is_leap(target_year, expected):
+    actual = MyYear(target_year).is_leap()
+
+    assert actual == expected
